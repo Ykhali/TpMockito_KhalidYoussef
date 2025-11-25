c20e3d6aec04d7cad832c8c873b0cf3ddc91dcd5
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-11-25 22:01:46 +0100

    2eme commit

---
+++ src/main/java/Carre.java
@@ -0,0 +1,13 @@
+public class Carre {
+    private double cote;
+    public Carre(double cote) {
+        this.cote = cote;
+    }
+    public double getCote() {
+        return cote;
+    }
+    public void setCote(double cote) {
+        this.cote = cote;
+    }
+    
+}
